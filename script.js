e211cae3aff7bbde6f9ffc03d6d06e0b42cf6a73
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-01-20 23:01:52 -0500

    fixed workday to reflect 8 hour day, allow user to delete text and save

--- script.js
+++ script.js
@@ -31,7 +31,7 @@
     // WHEN I open the planner
     // THEN the current day is displayed at the top of the calendar
     const display_time = moment().format("dddd, MMMM D, YYYY");
-    currentDay.textContent = `Today is ${display_time}`;
+    currentDay.textContent = `Today is ${display_time}.`;
     
     // const currTime = new Date().getHours();
     const currTime = parseInt(moment().format("H"));
@@ -41,14 +41,11 @@
     if (!schedule) {
         schedule = [];
     }
-    // const schedule = {
-        
-    // };
     
     // WHEN I scroll down
     // THEN I am presented with time blocks for standard business hours
     // WHEN I view the time blocks for that day
-    for (let i = 8; i <= 23; i++) {
+    for (let i = 8; i <= 17; i++) {
         const row = $('<div class="row time-block">');
         const hour = $('<div class="hour col-2">');
         const textarea = $('<textarea class="description col-8">');
@@ -75,15 +72,12 @@
         //const saveBtn = $(".saveBtn");
         $(".saveBtn").on("click", function(event){
             let userInput = textarea.val();
-            if (textarea.val() !== ""){
             schedule [thisHour] = userInput;
-            console.log (schedule);
             // THEN the text for that event is saved in local storage
-            localStorage.setItem("schedule", JSON.stringify(schedule));
             // WHEN I refresh the page
             // THEN the saved events persist
-            /////??????????????????????????
-            }
+            localStorage.setItem("schedule", JSON.stringify(schedule));
+    
         });  
 
         
@@ -95,33 +89,3 @@
        
      
 
-// var myArr = ["cat", "dog", "mastadon"];
-
-// console.log(myArr);
-// console.log(myArr[1]);
-// console.log(myArr["2"]);
-
-// var i = 0;
-// console.log(myArr[i]);
-// myArr[3] = "iguana";
-// i = 4;
-// myArr[i] = "bat";
-// console.log(myArr);
-
-// var myObj = {
-//   name: "Ben",
-//   job: "tutor",
-// };
-
-// console.log(myObj);
-// console.log(myObj.name);
-// console.log(myObj["job"]);
-
-// var key = "hasChildren";
-// myObj[key] = true;
-
-// key = "hasPets";
-// myObj[key] = true;
-
-// myObj["name"] = "Benjamin";
-// console.log(myObj);
